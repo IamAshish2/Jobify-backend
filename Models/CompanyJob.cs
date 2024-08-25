b7287594e242a40ffa5005b40e56935f5d7d8fb3
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-25 21:36:40 +0545

    company-job join table

---
+++ Models/CompanyJob.cs
@@ -0,0 +1,10 @@
+﻿namespace jobify_Backend.Models
+{
+    public class CompanyJob
+    {
+        public int? JobId { get; set; }
+        public Job Job { get; set; }
+        public int? CompanyId {  get; set; }
+        public Company Company { get; set; }
+    }
+}
