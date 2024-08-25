c9043e546a1a55319a0fe9b91f9ecaf537cca393
IamAshish2 <karkiaashish9899@gmail.com>
2024-08-25 16:13:15 +0545

    defined dto's for signup login and other use case

---
+++ Dto/UserDtos/GetUserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace jobify_Backend.Dto.UserDtos
+{
+    public class GetUserDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string JobTitle { get; set; }
+        public string Location { get; set; }
+        public string Bio { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConformPassword { get; set; }
+    }
+}
---
+++ Dto/UserDtos/SignupDto.cs
@@ -0,0 +1,10 @@
+﻿namespace jobify_Backend.Dto.UserDtos
+{
+    public class SignupDto
+    {
+        public string UserName { get; set; }
+        public string Email{ get; set; }
+        public string Password{ get; set; }
+        public  string Role { get; set; }
+    }
+}
